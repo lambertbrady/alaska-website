e8b852f7e4c9d0ed76679bf1c911c30ed9a25e5c
lambertbrady <lambertbrady14@gmail.com>
2017-01-15 21:40:03 -0600

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+    <title> Lambert Wilderness </title>
+    <link rel="stylesheet" href="vendors/normalize.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
+</head>
+<body>
+	<?php include 'header.php'?>
+	<div class="main">
+		<div class="wrapper wrapper-welcome">
+		    <div class="welcome text-primary-lightBackground">
+			<h2 class="text-primary-lightBackground">
+			    Welcome to the Wilderness
+			</h2>
+			<p>
+			    Here's some great information to help you figure this out.
+			</p>
+			<p>
+			Heyyyyy</p>
+			<p >Stuff about what we do.
+			</p>
+		    </div>
+		</div>
+		<div class="gallery-flex">
+		    <a href="/Gallery">
+			<div>
+			    <div>  </div>
+			    <div>  </div>
+			    <div>  </div>
+			</div>
+			<div>
+			    <div>  </div>
+			    <div>  </div>
+			    <div>  </div>
+			</div>
+		    </a>
+		</div>	
+	</div>
+	<?php include 'footer.php';?>
+</body> 
+</html>
