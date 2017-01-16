9c0dc74d446325f577608dfc3f1cb3648e5352aa
lambertbrady <lambertbrady14@gmail.com>
2017-01-15 21:54:14 -0600

    Update and rename 404.html to 404.php
    update to php file, include header.php and footer.php

--- 404.php
+++ 404.php
@@ -1,11 +1,10 @@
 <html>
-
 <head>
     <title>404 Page Not Found</title>
 </head>
-
+<?php include 'header.php'?>
 <body>
     <h2>Website under construction - we'll be up and running soon!</h2>
 </body>
-
+<?php include 'footer.php'?>
 </html>
