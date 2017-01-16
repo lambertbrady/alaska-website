b32959688319f7c97174e8d34be1aa0e2c945cc4
lambertbrady <lambertbrady14@gmail.com>
2017-01-15 21:33:13 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<html>
+  <head></head>
+  <div class="hero">
+    <div class="wrapper-hero-image">
+        <div class="hero-image"> </div>
+    </div>
+  <div class="gradient"> </div>
+    <div class="hero-flex">
+        <div class="logo">
+            <div>
+                <a href="/Home"> LOGO PLACEHOLDER (LAMBERT GUIDING SERVICE)</a>
+            </div>
+        </div>
+        <div class="nav-flex">
+            <a href="/About"> About </a>
+            <a href="/Gallery"> Gallery </a>
+            <a href="/Activities"> Activities </a>
+            <a href="/Contact"> Contact </a>
+        </div>
+    </div>
+	  <div class="hero-content">
+		  <h3 class="text-primary-darkBackground">
+			  This is a header. Be sure to check out font sizes.
+		  </h3>
+		  <a href="/Gallery" class="btn btn-large btn-hero"> View More </a>
+	  </div>
+  </div>
+</html>
