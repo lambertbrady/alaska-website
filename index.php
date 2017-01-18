8d7c03a797440e7cd203d7d749eff126cf21e5d2
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-18 17:12:33 -0600

    add stylesheet link to main.css

--- index.php
+++ index.php
@@ -3,45 +3,46 @@
 <head>
     <title> Lambert Wilderness </title>
     <link rel="stylesheet" href="vendors/normalize.css">
+    <link rel="stylesheet" href="main.css">
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.1/angular.min.js"></script>
 </head>
 
 <body>
     <?php include 'header.php'?>
-    <?php include 'hero.php'?>
-        <div id="main">
-            <div class="wrapper wrapper-welcome">
-                <div class="welcome text-primary-lightBackground">
-                    <h2 class="text-primary-lightBackground">
+        <?php include 'hero.php'?>
+            <div id="main">
+                <div class="wrapper wrapper-welcome">
+                    <div class="welcome text-primary-lightBackground">
+                        <h2 class="text-primary-lightBackground">
                     Welcome to the Wilderness
                 </h2>
-                    <p>
-                        Here's some great information to help you figure this out.
-                    </p>
-                    <p>
-                        Heyyyyy
-                    </p>
-                    <p>
-                        Stuff about what we do.
-                    </p>
+                        <p>
+                            Here's some great information to help you figure this out.
+                        </p>
+                        <p>
+                            Heyyyyy
+                        </p>
+                        <p>
+                            Stuff about what we do.
+                        </p>
+                    </div>
+                </div>
+                <div class="gallery-flex">
+                    <a href="/Gallery">
+                        <div>
+                            <div> </div>
+                            <div> </div>
+                            <div> </div>
+                        </div>
+                        <div>
+                            <div> </div>
+                            <div> </div>
+                            <div> </div>
+                        </div>
+                    </a>
                 </div>
             </div>
-            <div class="gallery-flex">
-                <a href="/Gallery">
-                    <div>
-                        <div> </div>
-                        <div> </div>
-                        <div> </div>
-                    </div>
-                    <div>
-                        <div> </div>
-                        <div> </div>
-                        <div> </div>
-                    </div>
-                </a>
-            </div>
-        </div>
-        <?php include 'footer.php';?>
+            <?php include 'footer.php';?>
 </body>
 
 </html>
