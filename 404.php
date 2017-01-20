279338dded54339e5dc6b3da626dd9f9e960f591
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-19 18:40:42 -0600

    add basic stylesheets to 404 - can remove later

--- 404.php
+++ 404.php
@@ -2,12 +2,14 @@
 
 <head>
     <title>404 Page Not Found</title>
+    <link rel="stylesheet" href="vendors/reset.css">
+    <link rel="stylesheet" href="vendors/normalize.css">
 </head>
 
 <body>
     <?php include 'header.php'?>
-    <h2>Website under construction - we'll be up and running soon!</h2>
-    <?php include 'footer.php'?>
+        <h2>Website under construction - we'll be up and running soon!</h2>
+        <?php include 'footer.php'?>
 </body>
 
 </html>
