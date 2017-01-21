d10bc6232d1f6191be97e089912b860a352364b5
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-20 18:05:51 -0600

    more header updates

--- header.php
+++ header.php
@@ -9,25 +9,13 @@
 
 <body>
     <div id="header">
-        <div id="hero">
-            <div class="wrapper-hero-image">
-                <div class="hero-image"> </div>
-            </div>
-            <div class="gradient"> </div>
-            <div class="hero-content">
-                <h3 class="text-primary-darkBackground">
-                    This is a header. Be sure to check out font sizes.
-                </h3>
-                <a href="/Gallery" class="btn btn-large btn-hero"> View More </a>
-            </div>
-        </div>
-        <div id="navbar" , class="flex-container">
+        <div id="navbar" , class="flex-container-row">
             <div class="logo">
                 <a href="/"> LOGO PLACEHOLDER (LAMBERT GUIDING SERVICE)</a>
             </div>
             <div class="flex-spacer"></div>
             <div class="nav">
-                <ul class="flex-container">
+                <ul class="flex-container-row">
                     <li>
                         <a href="/about"> About </a>
                     </li>
@@ -47,6 +35,18 @@
                         <a href="/contact"> Contact </a>
                     </li>
                 </ul>
+            </div>
+        </div>
+        <div id="hero">
+            <div class="wrapper-hero-image">
+                <div class="hero-image"> </div>
+            </div>
+            <div class="gradient"> </div>
+            <div class="hero-content">
+                <h3 class="text-primary-darkBackground">
+                    This is a header. Be sure to check out font sizes.
+                </h3>
+                <a href="/Gallery" class="btn btn-large btn-hero"> View More </a>
             </div>
         </div>
     </div>
