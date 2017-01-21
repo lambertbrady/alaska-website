183aa7faad1b3e37f89c366dada4e9f05911ab68
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-20 20:08:53 -0600

    finish header draft (header.php and header.css) - lots of flexbox

--- header.php
+++ header.php
@@ -13,44 +13,47 @@
             <div class="hero-image"> </div>
             <div class="hero-overlay"> </div>
         </div>
-        <div class="navbar flex-container-row">
-            <div class="logo">
-                <a href="/"> LOGO PLACEHOLDER (LAMBERT GUIDING SERVICE)</a>
+        <div class="flex-container-col">
+            <div class="navbar flex-container-row">
+                <div class="logo">
+                    <a href="/"> LOGO PLACEHOLDER (LAMBERT GUIDING SERVICE)</a>
+                </div>
+                <div class="flex-spacer"></div>
+                <div class="nav">
+                    <ul class="flex-container-row">
+                        <li>
+                            <a href="/about"> About </a>
+                        </li>
+                        <li>
+                            <a href="/destination"> Destination </a>
+                        </li>
+                        <li>
+                            <a href="/activities"> *Activities </a>
+                        </li>
+                        <li>
+                            <a href="/planning"> Planning </a>
+                        </li>
+                        <li>
+                            <a href="/gallery"> Gallery </a>
+                        </li>
+                        <li>
+                            <a href="/contact"> Contact </a>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="flex-spacer"></div>
-            <div class="nav">
-                <ul class="flex-container-row">
-                    <li>
-                        <a href="/about"> About </a>
-                    </li>
-                    <li>
-                        <a href="/destination"> Destination </a>
-                    </li>
-                    <li>
-                        <a href="/activities"> *Activities </a>
-                    </li>
-                    <li>
-                        <a href="/planning"> Planning </a>
-                    </li>
-                    <li>
-                        <a href="/gallery"> Gallery </a>
-                    </li>
-                    <li>
-                        <a href="/contact"> Contact </a>
-                    </li>
-                </ul>
+            <div class="page-title flex-container-col">
+                <div class="flex-spacer"></div>
+                <h3>
+                    This is a header. Be sure to check out font sizes.
+                </h3>
+                <p>Neat</p>
+                <div class="flex-spacer"></div>
+            </div>
+            <div class="scroll-button">
+                <a href="/Gallery" class="btn btn-large btn-hero"> Explore (below) </a>
             </div>
         </div>
-        <!--
-        <div class="site-title">
-            <h3 class="text-primary-darkBackground">
-                This is a header. Be sure to check out font sizes.
-            </h3>
-        </div>
-        <div class="btn scroll-down">
-            <a href="/Gallery" class="btn btn-large btn-hero"> View More </a>
-        </div>
--->
     </div>
 </body>
 
