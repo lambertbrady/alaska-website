b80fcf4e4e516bfc502c9853a14aebe6dc7881f9
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-22 19:19:54 -0600

    php files

--- index.php
+++ index.php
@@ -8,41 +8,40 @@
 </head>
 
 <body>
-    <?php include 'header.php'?>
-        <?php include 'hero.php'?>
-            <div id="main">
-                <div class="wrapper wrapper-welcome">
-                    <div class="welcome text-primary-lightBackground">
-                        <h2 class="text-primary-lightBackground">
-            Welcome to the Wilderness
-        </h2>
-                        <p>
-                            Here's some great information to help you figure this out.
-                        </p>
-                        <p>
-                            Heyyyyy
-                        </p>
-                        <p>
-                            Stuff about what we do.
-                        </p>
-                    </div>
-                </div>
-                <div class="gallery-flex">
-                    <a href="/gallery">
-                        <div>
-                            <div> </div>
-                            <div> </div>
-                            <div> </div>
-                        </div>
-                        <div>
-                            <div> </div>
-                            <div> </div>
-                            <div> </div>
-                        </div>
-                    </a>
-                </div>
+    <?php include 'header.php';?>
+    <div id="content">
+        <div class="wrapper wrapper-welcome">
+            <div class="welcome text-primary-lightBackground">
+                <h2 class="text-primary-lightBackground">
+                    Welcome to the Wilderness
+                </h2>
+                <p>
+                    Here's some great information to help you figure this out.
+                </p>
+                <p>
+                    Heyyyyy
+                </p>
+                <p>
+                    Stuff about what we do.
+                </p>
             </div>
-            <?php include 'footer.php';?>
+        </div>
+        <div class="gallery-flex">
+            <a href="/gallery">
+                <div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                </div>
+                <div>
+                    <div> </div>
+                    <div> </div>
+                    <div> </div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <?php include 'footer.php';?>
 </body>
 
 </html>
