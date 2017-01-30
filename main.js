378665dfeddddad1c8b7e3d8bc77cab770b83149
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2017-01-29 21:54:09 -0600

    js updates

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-var scrollBtnList = document.getElementsByClassName("btn-scroll");
+////function scrollAnimation() {
+////    alert("hello");
+////};
+//
 
-//function scrollAnimation() {
-//    alert("hello");
-//};
+function runScroll() {
+    scrollTo(document.documentElement, 0, 600);
+}
 
-function scrollAnimation(element, to, duration) {
+var scrollBtnList;
+scrollBtnList = document.querySelectorAll(".btn-scroll");
+
+for (var i = 0; i < scrollBtnList.length; i++) {
+    scrollBtnList[i].addEventListener("click", runScroll);
+}
+
+function scrollTo(element, to, duration) {
     if (duration <= 0) return;
     var difference = to - element.scrollTop;
     var perTick = difference / duration * 10;
@@ -14,8 +24,4 @@ function scrollAnimation(element, to, duration) {
         if (element.scrollTop == to) return;
         scrollTo(element, to, duration - 10);
     }, 10);
-}
-
-for (var i = 0; i < scrollBtnList.length; i++) {
-    scrollBtnList[i].addEventListener("click", scrollAnimation(element, 0, 600));
 }
